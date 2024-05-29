012c0e520d41e45dbeb072d3a1aa788da7963e78
alad1nks <vasya.sidr777@gmail.com>
2024-05-29 20:09:40 +0300

    Initial commit

---
+++ src/main/kotlin/com/alad1nks/dubovozki/DubovozkiBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.alad1nks.dubovozki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DubovozkiBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DubovozkiBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/alad1nks/dubovozki/DubovozkiBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alad1nks.dubovozki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DubovozkiBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
